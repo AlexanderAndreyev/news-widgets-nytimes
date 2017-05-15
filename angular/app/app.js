6711257e671b0857dadeb47e8c68215fb8e27120
Alexander <alexandr210993@gmail.com>
2017-05-15 17:38:27 +0300

    Finishing the implementation of the ng-widget

--- angular/app/app.js
+++ angular/app/app.js
@@ -1,7 +1,13 @@
 angular.module('newsWidgetApp', [])
 
-.controller('newsWidgetAppController', ['$scope', function($scope) {
+.controller('newsWidgetAppController', ['$scope', '$http', function($scope, $http) {
 
-  $scope.news = ['Alex', 'Dan', 'Max'];
-  
+  const apiKey = 'd25d034213e347749d132cc22e08adcc';
+  const url = "https://api.nytimes.com/svc/mostpopular/v2/mostviewed/Technology/30.json" + "?api-key=" + apiKey;
+
+  $http.get(url)
+    .then(function(response) {
+      $scope.news = response.data.results;
+    });
+
 }]);
